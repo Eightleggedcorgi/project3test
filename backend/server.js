b2b38736957b4fa73cc40909973857ac514a0684
Daniel Fordham <dfordham46@gmail.com>
2024-07-24 19:37:10 -0400

    actually started to write down the planning, also added method-override

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { PORT } = process.env;
 const cors = require('cors');
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const routes = require('./routes/index.js')
 
@@ -10,10 +11,11 @@ const routes = require('./routes/index.js')
 app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.use('/', routes)
-app.use((req, res) => {res.status(404).json({message: 'NOT A ROUTE'})})
+app.use((req, res) => {res.status(404).json({message: 'EMPTY ROUTE, TRY ANOTHER'})})
 
 // LISTENER
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
