e1b9aa020be2207cdc49bc73237ce4636b480b19
NickHurley90 <nickolaushurley@gmail.com>
2024-07-31 16:53:22 -0400

    Added images to Weather database

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
-//import "../App.css";
+import "../App.css";
 import { Link } from "react-router-dom";
+import weatherLogo from  '../assets/naturalDisasterIcon.png';
+
 
 const Header = (props) => {
   return (
       <nav className='nav'>
           <Link to="/">
-              <div>Home</div>
+              <div className="weatherLogo">
+              <img src={weatherLogo} alt={"S"} />
+              </div>
           </Link>
       </nav>
   );
